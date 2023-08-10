6b497711198437f7ab0e1b2b7d7d23e82f75618b
hedaya Mostafa <hedayaselim1807@gmail.com>
2023-08-10 12:56:47 +0200

    initial commit

---
+++ src/main/java/com/smart4aviation/flightInformation/FlightInformationApplication.java
@@ -0,0 +1,13 @@
+package com.smart4aviation.flightInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightInformationApplication.class, args);
+	}
+
+}
